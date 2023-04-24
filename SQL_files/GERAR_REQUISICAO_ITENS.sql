685521efee2313f7b0bafdd218ee89e5ea86ee8c
Matheus Theodoro <matheustheodor@hotmail.com>
2023-04-24 17:08:23 -0400

    identação da procedure e correção da data

--- SQL_files/GERAR_REQUISICAO_ITENS.sql
+++ SQL_files/GERAR_REQUISICAO_ITENS.sql
@@ -1,21 +1,46 @@
-SET TERM ^ ;
-
-create or alter procedure CADASTRAR_REQUISICAO_ITENS (
-    ID_CLIENTE          INTEGER,
-    ID_ITEM         INTEGER NOT NULL,
-    ENTREGAR        FLOAT
-    )
-as
-declare variable CD_REQ integer;
+CREATE OR ALTER PROCEDURE GERAR_REQUISICAO_ITENS (
+    ID_CLIENTE INTEGER,
+    ID_ITEM INTEGER NOT NULL,
+    ENTREGAR FLOAT
+)
+AS
 BEGIN
+    DECLARE VARIABLE CD_REQ INTEGER;
     
-    SELECT MAX(ID_REQUISICAO) FROM REQUISICOES INTO :CD_REQ;
+    SELECT MAX(ID_REQUISICAO) 
+    FROM REQUISICOES 
+    INTO :CD_REQ;
     
-    INSERT INTO REQUISICOES_ITENS (ID_REQUISICAO, ID_ITEM, ID_PRODUTO, DESCRICAO, QTDE, VALOR_UNITARIO, REF, DADOS, CODIGO_FAB, MARCA, CODIGO_BARRA, ENTREGAR, ENTREGUE, DATA_ENTREGA)
-                       VALUES (:CD_REQ, :ID_ITEM, NULL, :DESCRICAO, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-
+    INSERT INTO REQUISICOES_ITENS (
+        ID_REQUISICAO, 
+        ID_ITEM, 
+        ID_PRODUTO, 
+        DESCRICAO, 
+        QTDE, 
+        VALOR_UNITARIO, 
+        REF, 
+        DADOS, 
+        CODIGO_FAB, 
+        MARCA, 
+        CODIGO_BARRA, 
+        ENTREGAR, 
+        ENTREGUE, 
+        DATA_ENTREGA
+    ) 
+    VALUES (
+        :CD_REQ, 
+        :ID_ITEM, 
+        NULL, 
+        NULL, 
+        NULL, 
+        NULL, 
+        NULL, 
+        NULL, 
+        NULL, 
+        NULL, 
+        NULL, 
+        :ENTREGAR, 
+        NULL, 
+        NULL
+    );
 END^
-
-SET TERM ; ^
-
-
